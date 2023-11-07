a3d706f8aec201f7432e4c6e9bc12efae81f6563
barun <barunps3@gmail.com>
2023-11-07 20:22:05 +0100

    added onsubmit funciton login form

--- ehr-webapp/app/components/LoginCard.tsx
+++ ehr-webapp/app/components/LoginCard.tsx
@@ -1,9 +1,19 @@
+'use client'
 import React from "react"
 import styles from './LoginCard.module.css'
+import { useRouter } from 'next/navigation'
+
+
 
 export default function LoginCard() {
+  const router = useRouter()
+  const handleClick = (e: React.FormEvent) => {
+      e.preventDefault();
+      return router.push('/search')
+    }
+
   return (
-    <form className={styles.loginForm}>
+    <form onSubmit={handleClick} className={styles.loginForm}>
       <div>
         <label className={styles.label} htmlFor="username">Username</label>
         <input className={styles.input} type="text" id="username" name="username" required />
