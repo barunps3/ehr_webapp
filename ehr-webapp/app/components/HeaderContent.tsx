ea57d5c35544a85679d86a5e9b4ab5b2fdf9fd9b
barun <barunps3@gmail.com>
2023-11-08 09:11:31 +0100

    cleanup

--- ehr-webapp/app/components/HeaderContent.tsx
+++ ehr-webapp/app/components/HeaderContent.tsx
@@ -5,10 +5,10 @@ export default function HeaderContent() {
   return (
     <>
       <div>
-        <img className="company-icon" src="/ehr.png" alt="ehr icon" />
+        <img className={styles.companyIcon} src="/ehr.png" alt="ehr icon" />
       </div>
       <div>
-        <h3 className="company-name">Baum Electronic Health Records</h3>
+        <h3 className={styles.companyName}>Baum Electronic Health Records</h3>
       </div>
     </>
   )
