83050ba81b2a9c9ba6283185e805b58c1211f11f
barun <barunps3@gmail.com>
2023-11-24 08:18:20 +0100

    fixed view of search result

--- ehr-webapp/app/components/searchResultCard.tsx
+++ ehr-webapp/app/components/searchResultCard.tsx
@@ -10,27 +10,34 @@ function ResultRow({ patient }: {patient: searchResult}) {
   const router = useRouter();
   console.log("value in result row:", patient)
   return (
-      <Link href={{
-        pathname: '/search/patient',
-        query: {
-          idType: `${patient.IdType}`,
-          idValue: `${patient.IdValue}`,
-        }
-      }}>
-        <div className="rowCell">{patient.FirstName} {patient.LastName}</div>
-        <div className="rowCell">{patient.Gender}</div>
-        <div className="rowCell">{patient.DateOfBirth}</div>
-        <div className="rowCell">{patient.IdType}</div>
-        <div className="rowCell">{patient.IdValue}</div>
-      </Link>
+      // <Link href={{
+      //   pathname: '/search/patient',
+      //   query: {
+      //     idType: `${patient.IdType}`,
+      //     idValue: `${patient.IdValue}`,
+      //   }
+      // }}>
+      //   <div className="rowCell">{patient.FirstName} {patient.LastName}</div>
+      //   <div className="rowCell">{patient.Gender}</div>
+      //   <div className="rowCell">{patient.DateOfBirth}</div>
+      //   <div className="rowCell">{patient.IdType}</div>
+      //   <div className="rowCell">{patient.IdValue}</div>
+      // </Link>
 
-    // <tr key={patient.IdValue} onClick={() => router.push(`/search/patient?idType=${patient.IdType}&idValue=${patient.IdValue}`)}>
-    //   <td>{patient.FirstName} {patient.LastName}</td>
-    //   <td>{patient.Gender}</td>
-    //   <td>{patient.DateOfBirth}</td>
-    //   <td>{patient.IdType}</td>
-    //   <td>{patient.IdValue}</td>
-    // </tr>
+    <tr key={patient.IdValue} onClick={() => router.push(`/search/patient?idType=${patient.IdType}&idValue=${patient.IdValue}`)}>
+      <td>{patient.FirstName} {patient.LastName}</td>
+      <td>{patient.Gender}</td>
+      <td>{patient.DateOfBirth}</td>
+      <td>{patient.IdType}</td>
+      <td>{patient.IdValue}</td>
+      <td><Link href={{
+          pathname: '/search/patient',
+          query: {
+            idType: `${patient.IdType}`,
+            idValue: `${patient.IdValue}`,
+          }
+        }}>Link</Link></td>
+    </tr>
   )
 }
 
@@ -40,22 +47,22 @@ export default function SearchResultCard({ searchResult }: { searchResult: searc
   if (searchResult.length > 0) {
     return (
       <div className={styles.resultsContainer}>
-        <div className="table">
+        {/* <div className="table">
           <div className="headerCell">Name</div>
           <div className="headerCell">Gender</div>
           <div className="headerCell">Date of Birth</div>
           <div className="headerCell">ID Type</div>
           <div className="headerCell">ID Value</div>
         </div>
-        {patients}
-        {/* <table>
+        {patients} */}
+        <table>
           <tbody>
             <tr className={styles.headerRow}>
-              <td>Name</td><td>Gender</td><td>Date of Birth</td><td>ID Type</td><td>ID value</td>
+              <td>Name</td><td>Gender</td><td>Date of Birth</td><td>ID Type</td><td>ID value</td><td>View</td>
             </tr>
             {patients}
           </tbody>
-        </table> */}
+        </table>
       </div>
     )
   }
