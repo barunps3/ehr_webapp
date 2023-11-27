bf2a1da0932783603aaa952b4950335b3d727b00
barun <barunps3@gmail.com>
2023-11-27 08:54:37 +0100

    fixed patientpage base

--- ehr-webapp/app/components/HeaderContent.tsx
+++ ehr-webapp/app/components/HeaderContent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from './styles/HeaderContent.module.css'
 import { PAGENAME } from "../utils/constants";
+import Link from "next/link";
 
 function HeaderWithOnlyName() {
   return (
@@ -15,10 +16,34 @@ function HeaderWithOnlyName() {
   )
 }
 
+function HeaderWithNav() {
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.rightAligned}>
+        <img className={styles.companyIcon} src="/ehr.png" alt="ehr icon" />
+        <div>
+          <p id={styles.baum}>Baum</p>
+          <p id={styles.ehr}>EHR</p>
+        </div>
+        <Link className="search" href="/search" target="_blank">
+          <button id={styles.searchPageBtn} type="button">Search</button>
+        </Link>
+      </div>
+      <div className="leftAligned">
+        <div className={styles.userDropdown} data-dropdown>
+          <button>
+            <img src="/personCircle.svg" alt="Your Account" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function HeaderContent({ pageName }: {pageName: PAGENAME}) {
-  if (pageName === PAGENAME.LoginPage) {
+  if (pageName === PAGENAME.PatientHistory) {
     return ( 
-      <HeaderWithOnlyName />
+      <HeaderWithNav />
     )
   }
   return <HeaderWithOnlyName />
