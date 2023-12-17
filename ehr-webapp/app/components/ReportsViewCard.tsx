2cf9db63d5d3ef9585ea3cc8e74e46fa63cc02fb
barun <barunps3@gmail.com>
2023-12-17 19:06:51 +0100

    removed additional report

--- ehr-webapp/app/components/ReportsViewCard.tsx
+++ ehr-webapp/app/components/ReportsViewCard.tsx
@@ -1,5 +1,41 @@
+'use client'
 import styles from './styles/PatientReportViewCard.module.css'
-import Report from './Report'
+import ImageGallery from "./utils/ImageGallery"
+import CommentsCard from "./utils/CommentsCard"
+import { getReports } from "../lib/fetches"
+import { useEffect, useState } from "react"
+
+const testImages = [
+  "/localImages/lumbar-spine-front.png",
+  "/localImages/lumbar-spine-side.png"
+]
+
+export function Report({ patientUUID, reportType, showReport }: 
+  {patientUUID: string, reportType: string, showReport: string}) {
+  const [fetchedImages, setImages] = useState<string[]>([])
+
+  useEffect(() => {
+    async function fetchImages() {
+      const images = []
+      const reports = await getReports(patientUUID, reportType, showReport) 
+      for (const report of reports) {
+        images.push(report["BlobUrl"])
+      }
+      setImages(images)
+    }
+    fetchImages()
+  }, [])
+
+  console.log("blob Images", fetchedImages)
+  if (fetchedImages.length != 0) {
+    return (
+      <div className={styles.reportContainer}>
+        <ImageGallery defaultImages={fetchedImages}/>
+        <CommentsCard />
+      </div>
+    )
+  }
+}
 
 type ReportsViewCard = {
   patientUUID: string
