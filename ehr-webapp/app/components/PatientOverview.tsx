c03005dc6b2f34847a63ebc6b749ac3ffa740e44
barun <barunps3@gmail.com>
2023-12-28 15:34:16 +0100

    refactor

--- ehr-webapp/app/components/PatientOverview.tsx
+++ ehr-webapp/app/components/PatientOverview.tsx
@@ -2,42 +2,13 @@
 import ExplorerSideBar from "./ExplorerSidebar";
 import { PatientBasicInfo, ReportFiles } from "../utils/dataTypes";
 import styles from './styles/PatientReportViewCard.module.css'
-import ImageGallery from "./utils/ImageGallery"
-import CommentsCard from "./utils/CommentsCard"
-import { getReports } from "../lib/fetches"
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { Report } from "./utils/Report";
 
 const testImages = [
   "/localImages/lumbar-spine-front.png",
   "/localImages/lumbar-spine-side.png"
 ]
-
-export function Report({ patientUUID, reportType, showReport }: 
-  {patientUUID: string, reportType: string, showReport: string}) {
-  const [fetchedImages, setImages] = useState<string[]>([])
-
-  useEffect(() => {
-    async function fetchImages() {
-      const images = []
-      const reports = await getReports(patientUUID, reportType, showReport) 
-      for (const report of reports) {
-        images.push(report["BlobUrl"])
-      }
-      setImages(images)
-    }
-    fetchImages()
-  }, [])
-
-  console.log("blob Images", fetchedImages)
-  if (fetchedImages.length != 0) {
-    return (
-      <div className={styles.reportContainer}>
-        <ImageGallery defaultImages={fetchedImages}/>
-        <CommentsCard />
-      </div>
-    )
-  }
-}
 
 export default function PatientOverview(
   { reportsList, data }: {reportsList: ReportFiles, data: PatientBasicInfo}) {
---
+++ ehr-webapp/app/components/utils/Report.tsx
@@ -0,0 +1,32 @@
+import ImageGallery from "./ImageGallery"
+import CommentsCard from "./CommentsCard"
+import { getReports } from "@/app/lib/fetches"
+import { useEffect, useState } from "react"
+import styles from '../styles/Report.module.css'
+
+export function Report({ patientUUID, reportType, showReport }: 
+  {patientUUID: string, reportType: string, showReport: string}) {
+  const [fetchedImages, setImages] = useState<string[]>([])
+
+  useEffect(() => {
+    async function fetchImages() {
+      const images = []
+      const reports = await getReports(patientUUID, reportType, showReport) 
+      for (const report of reports) {
+        images.push(report["BlobUrl"])
+      }
+      setImages(images)
+    }
+    fetchImages()
+  }, [])
+
+  console.log("blob Images", fetchedImages)
+  if (fetchedImages.length != 0) {
+    return (
+      <div className={styles.reportContainer}>
+        <ImageGallery defaultImages={fetchedImages}/>
+        <CommentsCard />
+      </div>
+    )
+  }
+}
